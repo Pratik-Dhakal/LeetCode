058c7beaa1fbd6e95b248eae6d9d4579ff375017
Pratik-Dhakal <dhakalpratik96@gmail.com>
2020-03-12 02:46:39 -0500

    lets see where it commits

---
+++ line.c
@@ -0,0 +1,11 @@
+int find_filename(int n, char **b)
+{
+    int i;
+    int counter = 0; /*equals -1 if didnt find*/
+    int check = 0;
+
+    for(i = 0;i<n&&check==0;i++)
+    {
+        if(strcmp())
+    }
+}
